532fe51ae5135fec649349d631baf1a5a8dfa4d7
nicholasjhall <njh.hall@gmail.com>
2020-08-27 13:13:09 -0400

    added karma per word dictionary

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, redirect, url_for, render_template, request
 import praw
+import nltk
+# nltk.download('punkt') is needed
+
 app = Flask(__name__)
 reddit = praw.Reddit("scanner")
+karma_dict = {}
 
 
 @app.route("/", methods=["POST", "GET"])
@@ -20,8 +24,17 @@ def subredditPage(sub):
 	if request.method == "GET":
 		print(sub)
 		subreddit = reddit.subreddit(sub)
-		for submission in subreddit.hot(limit=1):
+		for submission in subreddit.top(limit=50):
 			print(submission.title)
+			words = nltk.word_tokenize(submission.title)
+			karma = submission.score
+			tagged = nltk.pos_tag(words)
+			for word in tagged:
+				if word in karma_dict:
+					karma_dict[word] += submission.score
+				else:
+					karma_dict[word] = submission.score
+		print(karma_dict)
 		return render_template("subreddit.html", sub=sub)
 	else:
 		return render_template("subreddit.html", sub=sub)
